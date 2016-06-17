773c94ec6ff657f4729af61a89c5334508991f4f
Arunalu Kariyawasam <arunalu.kariyawasam@tripspark.com>
2016-06-17 09:59:14 -0700

    Adding additional currency types

--- denomCounter.py
+++ denomCounter.py
@@ -1,13 +1,19 @@
 import csv
-# with open('event_transaction_109_2.csv', newline='') as csvFile:
-# 	fileReader = csv.reader(csvFile, delimiter=' ', quotechar='|')
 
 pennyCount = 0
 nickelCount = 0 
 dimeCount = 0 
 quarterCount = 0 
+fiftyCentCount = 0
+fiveDollarCount = 0
+tenDollarCount = 0
+twentyDollarCount = 0
+fiftyDollarCount = 0
+hundredDollarCount = 0
 dollarCount = 0 
 twoDollarCount = 0
+tokenCount = 0
+
 
 inputFile = input("Please enter the name of a csv file (don't forget the extension): ");
 
@@ -16,19 +22,31 @@ try:
 	fileReader = csv.reader(csvFile)
 	for row in fileReader:
 		if(row[3] == "Penny"):
-			pennyCount = pennyCount + 1
+			pennyCount += 1
 		elif (row[3] == "Nickel"):
-			nickelCount = nickelCount + 1
+			nickelCount += 1
 		elif (row[3] == "Dime"):
-			dimeCount = dimeCount + 1
+			dimeCount += 1
 		elif (row[3] == "Quarter"):
-			quarterCount = quarterCount + 1
+			quarterCount += 1
+		elif (row[3] == "Fifty Cent"):
+			fiftyCentCount += 1
 		elif ((row[3] == "Dollar") or(row[3] == "Dollar Coin")):
-			dollarCount = dollarCount + 1
+			dollarCount += 1
 		elif ((row[3] == "Two Dollar Coin") or (row[3] == "Two Dollar")):
-			twoDollarCount = twoDollarCount + 1
-		else:
-			print(row[3])
+			twoDollarCount += 1
+		elif (row[3] == "Five Dollar"):
+			fiveDollarCount += 1
+		elif (row[3] == "Ten Dollar"):
+			tenDollarCount += 1
+		elif (row[3] == "Twenty Dollar"):
+			twentyDollarCount += 1
+		elif (row[3] == "Fifty Dollar"):
+			fiftyDollarCount += 1
+		elif (row[3] == "Hundred Dollar"):
+			hundredDollarCount += 1
+		elif (row[3] == "Token"):
+			tokenCount += 1
 	#insert all functionality here
 
 	print("# of Pennies: ", pennyCount)
@@ -36,10 +54,18 @@ try:
 	print("# of Dimes: ", dimeCount)
 	print("# of Quarters: ", quarterCount)
 	print("# of Dollars: ", dollarCount)
-	print("# of Two Dollar Coins: ", twoDollarCount)
+	print("# of Two Dollars: ", twoDollarCount)
+	print("# of Five Dollar Bills: ", fiveDollarCount)
+	print("# of Ten Dollar Bills: ", tenDollarCount)
+	print("# of Twenty Dollar Bills: ", twentyDollarCount)
+	print("# of Fifty Dollar Bills: ", fiftyDollarCount)
+	print("# of Hundred Dollar Bills: ", hundredDollarCount)
+	print("# of Tokens: ", tokenCount)
 
 	totalValue = (pennyCount*0.01) + (nickelCount*0.05) + (dimeCount*0.10) + \
-				 (quarterCount*0.25) + (dollarCount*1) + (twoDollarCount*2)
+				 (quarterCount*0.25) + (dollarCount*1) + (twoDollarCount*2) + \
+				 (fiveDollarCount*5) + (tenDollarCount*10) + (twentyDollarCount*20) + \
+				 (fiftyDollarCount*50) + (hundredDollarCount*100)
 finally:
 	csvFile.close()
 
